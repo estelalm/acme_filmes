f3be6f8c7da55323d96e9cfb10c158058bcc64c8
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-02-29 17:16:48 -0300

    iniciado

---
+++ js/filmes.js
@@ -0,0 +1,18 @@
+
+export async function getFilmes (){
+
+    const url = 'http://localhost:8080/v2/AcmeFilmes/filmes'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.filmes
+}
+
+export async function getFilme (id){
+
+    const url = `http://localhost:8080/v2/AcmeFilmes/filme/${1}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.filme[0]
+}
