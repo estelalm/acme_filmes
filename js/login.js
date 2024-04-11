10af88d4e37db309bf532bdde9c70a1c0f16338a
Estela Alves de Moraes <esteela.almoraes@gmail.com>
2024-04-11 07:21:16 -0300

    conta

---
+++ js/login.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const botaoEntra = document.getElementById('botao-entrar')
+botaoEntra.addEventListener('click', () => {
+    window.location.assign('./pages/home.html')
+})
---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict'
+
+
+import { getFilmes, getFilme } from "./filmes.js"
+
+
+async function criarCard(filme) {
+
+    const card = document.createElement('div')
+    const titulo = document.createElement('h2')
+    titulo.textContent = filme.nome
+
+    const sinopse = document.createElement('textarea')
+    sinopse.textContent = filme.sinopse
+
+    card.replaceChildren(titulo, sinopse)
+
+    return card
+}
+
+
+async function preencherContainer(){
+    const container = document.querySelector('section')
+    const filmes = await getFilmes()
+
+    filmes.forEach(filme =>{
+        const card = criarCard(filme)
+        container.appendChild(card)
+    })
+}
+
+preencherContainer()
